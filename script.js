ca2ab2347a71b6ddc5790009ae87341fb611c193
DaniilOlshanskyi <danil2477@gmail.com>
2020-08-13 15:57:29 -0600

    First commit - header done

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function(){
+
+  // var dif;
+  // var request = $.ajax({
+  //         type: 'GET',
+  //         url: 'rest.php',
+  //         dataType: "json",
+  //         success: function(data) {
+  //            // $("#loglog").append(data.file_contents);
+  //            console.log(data.dif);
+  //            dif = data.dif;
+  //            if (dif<0){
+  //              $("#dif").append("Данил должен " + Math.abs(dif));
+  //            } else {
+  //              $("#dif").append("Денис должен " + Math.abs(dif));
+  //            }
+  //
+  //            var log = data.file_contents;
+  //            //var log = "05/10/2018 05:36:22pm Данил заплатил Изнач /n05/10/2018 05:36:34pmДенис заплатил /n";
+  //            for (var i=0; i<log.length; i++){
+  //              console.log(log.charAt(i));
+  //              if(log.charAt(i)=='/') {
+  //                if (log.charAt(i+1)=='n'){
+  //                  $("#loglog").append("<br>");
+  //                  i++;
+  //                } else {
+  //                  $("#loglog").append(log.charAt(i));
+  //                }
+  //              } else{
+  //                $("#loglog").append(log.charAt(i));
+  //              }
+  //            }
+  //         }
+  //     });
+  //
+  //   request.done(function( msg ) {
+  //   });
+  //   request.fail(function( jqXHR, textStatus ) {
+  //     alert( "Request failed: " + textStatus );
+  //   });
+  //
+  //   $("#rad_de_w").click(function(){
+  //     $("#rad_de").prop('checked', true);
+  //     $("#rad_da").prop('checked', false);
+  //   })
+  //
+  //   $("#rad_da_w").click(function(){
+  //     $("#rad_da").prop('checked', true);
+  //     $("#rad_de").prop('checked', false);
+  //   })
+
+});
